44ccb14d26e506067eb685936015d37fc28403ee
developerc <developerc@yandex.ru>
2024-04-02 22:38:37 +0300

    next commit

--- grpc/go.mod
+++ grpc/go.mod
@@ -1,12 +1,10 @@
-module github.com/developerc/finprojorchestr3
+module grpc
 
 go 1.22.1
 
-replace grpc => ./grpc/
-
 require (
+	github.com/developerc/finprojorchestr3 v0.0.0-20240402193307-4505ac6af433
 	google.golang.org/grpc v1.62.1
-	google.golang.org/protobuf v1.33.0
 )
 
 require (
@@ -15,5 +13,5 @@ require (
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240123012728-ef4313101c80 // indirect
-	grpc v0.0.0-00010101000000-000000000000 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 )
--- grpc/server/srv.go
+++ grpc/server/srv.go
@@ -1,1 +1,88 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"sync"
+
+	pb "github.com/developerc/finprojorchestr3/proto"
+	"google.golang.org/grpc"
+)
+
+var RegisteredAgentMap map[int]Agent //хранилище зарегистрированных агентов
+var IdAgent int                      //счетчик ID агентаvvv
+var mutex sync.Mutex
+
+type Agent struct {
+	Id   int    `json:"id"`
+	Ip   string `json:"ip"`
+	Port int    `json:"port"`
+	//mutex sync.Mutex
+}
+
+type Server struct {
+	pb.OrchServerServiceServer // сервис из сгенерированного пакета
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+func (s *Server) SendTask(ctx context.Context, task *pb.Task) (*pb.Task, error) {
+	return task, nil
+}
+
+func (s *Server) PushFinishTask(ctx context.Context, task *pb.Task) (*pb.Task, error) {
+
+	return task, nil
+}
+
+/*func (s *Server) HBreq(ctx context.Context, heartBit *pb.HeartBit) (*pb.HeartBitResp, error) {
+	//hbr := pb.HeartBitResp
+	return &pb.HeartBitResp, nil
+}*/
+
+// обрабатываем запрос на регистрацию агента
+func (s *Server) RegisterNewAgent(ctx context.Context, in *pb.AgentParams) (*pb.AgentParamsResponse, error) {
+	var agent Agent = Agent{}
+
+	mutex.Lock()
+	IdAgent++
+	agent.Id = IdAgent
+	agent.Ip = in.Ip
+	agent.Port = int(in.Port)
+	RegisteredAgentMap[IdAgent] = agent
+	mutex.Unlock()
+	log.Println("RegisteredAgentMap: ", RegisteredAgentMap)
+	return &pb.AgentParamsResponse{Id: int32(IdAgent)}, nil
+}
+
+func CreateOrchGRPCserver() {
+	RegisteredAgentMap = make(map[int]Agent)
+	host := "localhost"
+	port := "5000"
+
+	addr := fmt.Sprintf("%s:%s", host, port)
+	lis, err := net.Listen("tcp", addr) // будем ждать запросы по этому адресу
+
+	if err != nil {
+		log.Println("error starting tcp listener: ", err)
+		os.Exit(1)
+	}
+
+	log.Println("tcp listener started at port: ", port)
+	// создадим сервер grpc
+	grpcServer := grpc.NewServer()
+	// объект структуры, которая содержит реализацию серверной части OrchServerServiceServer
+	orchserverServiceServer := NewServer()
+	// зарегистрируем нашу реализацию сервера
+	pb.RegisterOrchServerServiceServer(grpcServer, orchserverServiceServer)
+	// запустим grpc сервер
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Println("error serving grpc: ", err)
+		os.Exit(1)
+	}
+}
